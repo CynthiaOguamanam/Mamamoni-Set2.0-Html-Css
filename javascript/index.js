f7d831bc190c83efe31989558de8f58e629d5038
CynthiaOguamanam <cynthiadymphna04@gmail.com>
2023-11-07 14:25:19 +0100

    set02-javascript

---
+++ javascript/index.js
@@ -0,0 +1,50 @@
+let age = 2;
+let name1 = "Cynthia";
+console.log(age);
+console.log(name1);
+
+let Para = "Hello world"
+// 3 ways of declaring a variable : 
+// let, Const, Var;
+
+// Data Types
+// Primitive and non-primitive data types
+// Primitive:
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4.Null
+// 5.Object
+// 6.Undefined
+// 7.BigInt...
+
+// 1..Array
+
+
+// String Data Type:
+// A String is a sequence of zero or more characters. A string begins and ends with 
+// a quote either single or double quote.
+// "9" '9' " " "Cynthia" "abcdefghijklmnopqrstuvwxyz"
+
+let Paragraph = "Hello and welcome to javascript";
+console.log(Paragraph);
+
+const School = "Day spring on high 556 67 bint'l school"
+console.log(School);
+//DECLARATION, VARAIBLE, VALUE
+let Class =  "pry 2";
+//Result
+console.log(Class);
+
+const username = prompt("What is your name?");
+let userAge = prompt(`Hi ${username}, how old are you?`);
+// let userAge2 = prompt("Hi ${username}, how old are you?")
+
+let Helen = 87;
+console.log(Helen);
+
+const Food = "Yam, egg, fish";
+console.log(Food);
+
+let a = "Zainab";
+// let b = 
---
+++ javascript/index2.js
@@ -0,0 +1,13 @@
+let school = "Apex + International + 2";
+console.log(school);
+
+let a = "5";
+let b = 5;
+let c = a + b;
+console.log(c);
+
+// Javascript Operators
+//Arithmetic operators: +, -, *, /, %, ++, --;
+// They perform arithmetics on numbers
+
+
